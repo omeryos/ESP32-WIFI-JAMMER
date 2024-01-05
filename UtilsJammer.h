d7bd4d35412f6bd79667cc2d9e226e06b3e46bb1
Omer <omer@ivory-sw.com>
2024-01-05 23:51:36 +0200

    second commit to verify git working

---
+++ UtilsJammer.h
@@ -0,0 +1,9 @@
+// UtilsJammer.h
+#ifndef UtilsJammer_h
+#define UtilsJammer_h
+
+void printAll(char *printText);
+void displayActiveChannels();
+bool isChannelAlreadyDetected(int channel);
+
+#endif
